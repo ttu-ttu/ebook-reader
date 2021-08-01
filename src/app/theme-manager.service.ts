09aca1fdf034b25b5911169e2725b5c58b85d852
ッツ <83872826+ttu-ttu@users.noreply.github.com>
2021-08-02 02:54:12 +0800

    Reorder theme selections

--- src/app/theme-manager.service.ts
+++ src/app/theme-manager.service.ts
@@ -17,10 +17,10 @@ export class ThemeManagerService {
 
   readonly availableThemes = [
     'light-theme',
-    'gray-theme',
-    'dark-theme',
     'ecru-theme',
     'water-theme',
+    'gray-theme',
+    'dark-theme',
     'black-theme',
   ];
 
