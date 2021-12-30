b5ec731241dc862d4cda6fe817219f1a9f57fd85
ChaseOxide <15276837+ChaseOxide@users.noreply.github.com>
2021-12-30 23:18:06 +0800

    fix: add missing module imports

--- src/app/message-dialog/message-dialog.module.ts
+++ src/app/message-dialog/message-dialog.module.ts
@@ -4,13 +4,15 @@
  * All rights reserved.
  */
 
+import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MessageDialogComponent } from './message-dialog.component';
 
 @NgModule({
   declarations: [MessageDialogComponent],
-  imports: [CommonModule],
-  exports: [MessageDialogComponent],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, A11yModule],
 })
 export class MessageDialogModule {}
