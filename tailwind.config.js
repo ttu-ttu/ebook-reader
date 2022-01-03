17587bea6c410f29c2bd180ed0bd61dbcb8190b5
ChaseOxide <15276837+ChaseOxide@users.noreply.github.com>
2022-01-03 18:00:58 +0800

    chore(deps): update tailwindcss to v3.0.8

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,24 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './src/app/**/*.{html,ts}',
-    ],
-  },
+  content: ['./src/app/**/*.{html,ts}'],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          'Noto Sans JP',
-          'sans-serif',
-        ],
-        serif: [
-          'Noto Serif JP',
-          'serif',
-        ],
+        sans: ['Noto Sans JP', 'sans-serif'],
+        serif: ['Noto Serif JP', 'serif'],
       },
       height: {
-        '21': '5.25rem',
+        21: '5.25rem',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
-    require('tailwindcss-elevation')(['hover']),
-    plugin(function({ addUtilities }) {
+    require('tailwindcss-elevation')(),
+    plugin(function ({ addUtilities }) {
       const newUtilities = {
         '.elevation-transition': {
           transition: 'box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1)',
