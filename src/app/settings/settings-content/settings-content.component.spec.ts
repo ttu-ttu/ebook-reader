f137c11fd5e7353ec8bffd2bb42d589ed9b46ffc
ChaseOxide <15276837+ChaseOxide@users.noreply.github.com>
2022-03-25 01:50:56 +0800

    refactor: inject window to allow mock for test

--- src/app/settings/settings-content/settings-content.component.spec.ts
+++ src/app/settings/settings-content/settings-content.component.spec.ts
@@ -6,12 +6,26 @@
 
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { WINDOW } from 'src/app/utils/dom-tokens';
 import { SettingsContentComponent } from './settings-content.component';
 
 describe('SettingsContentComponent', () => {
   let spectator: Spectator<SettingsContentComponent>;
   const createComponent = createComponentFactory({
     component: SettingsContentComponent,
+    providers: [
+      {
+        provide: WINDOW,
+        useValue: {
+          navigator: {
+            storage: {
+              persisted: async () => false,
+              persist: async () => true,
+            },
+          },
+        },
+      },
+    ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
   });
 
