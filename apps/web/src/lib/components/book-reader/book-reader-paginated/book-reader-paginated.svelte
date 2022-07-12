390f59db3675ff472e5bbf418d3703f24fd299f6
Master3008 <55414793+Master3008@users.noreply.github.com>
2022-07-12 19:54:49 +0200

    fix: fix horizontal paginated view on webkit browser (#175)
    closes #170

--- apps/web/src/lib/components/book-reader/book-reader-paginated/book-reader-paginated.svelte
+++ apps/web/src/lib/components/book-reader/book-reader-paginated/book-reader-paginated.svelte
@@ -383,6 +383,7 @@
   style:--book-content-hint-furigana-shadow-color={hintFuriganaShadowColor}
   style:--book-content-child-width="{width}px"
   style:--book-content-child-height="{height}px"
+  style:--book-content-child-column-width={!verticalMode && columnCount === 1 ? `${width}px` : ''}
   style:--book-content-column-count={columnCount}
   style:--book-content-image-max-width="{verticalMode
     ? width
@@ -432,6 +433,7 @@
 
   .book-content-container {
     column-count: var(--book-content-column-count, 1);
+    column-width: var(--book-content-child-column-width, auto); // required for WebKit + column-count 1
     column-gap: 20px;
     column-fill: auto;
     height: var(--book-content-child-height, 95vh);
