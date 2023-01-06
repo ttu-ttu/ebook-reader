bf949b8fcb55cb0721ccce21fc254c8b8360ef22
Renji-xD <trashmefull@hotmail.com>
2023-01-06 02:59:31 +0100

    fix(deps): remove duplicate prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [require('prettier-plugin-tailwindcss'), require('prettier-plugin-svelte')],
   singleQuote: true,
   trailingComma: 'none',
   printWidth: 100
