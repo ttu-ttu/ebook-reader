2c3bca86d5132fcc75942eb34c807530b52dc43b
Renji-xD <Renji-XD@users.noreply.github.com>
2023-01-09 13:21:54 +0100

    feat: local font selection (#207)

---
+++ apps/web/src/lib/components/settings/settings-font-selector.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { faFont } from '@fortawesome/free-solid-svg-icons';
+  import Popover from '$lib/components/popover/popover.svelte';
+  import { LocalFont } from '$lib/data/fonts';
+  import { dummyFn } from '$lib/functions/utils';
+  import Fa from 'svelte-fa';
+
+  export let availableFonts: LocalFont[] = [LocalFont.NOTOSANSJP];
+  export let fontValue: string;
+
+  let element: Popover;
+</script>
+
+<Popover bind:this={element} placement="bottom">
+  <Fa icon={faFont} slot="icon" class="mx-2" />
+  <div slot="content">
+    {#each availableFonts as font (font)}
+      <div
+        role="button"
+        class="px-4 py-2 hover:bg-gray-900"
+        on:click={() => {
+          fontValue = font;
+          element.toggleOpen();
+        }}
+        on:keyup={dummyFn}
+      >
+        {font}
+      </div>
+    {/each}
+  </div>
+</Popover>
---
+++ apps/web/src/lib/data/fonts.ts
@@ -0,0 +1,13 @@
+/**
+ * @license BSD-3-Clause
+ * Copyright (c) 2022, ッツ Reader Authors
+ * All rights reserved.
+ */
+
+export enum LocalFont {
+  GENREI = 'Genrei Koburi Mincho v5',
+  KLEEONE = 'Klee One',
+  NOTOSANSJP = 'Noto Sans JP',
+  NOTOSERIFJP = 'Noto Serif JP',
+  SHIPPORIMINCHO = 'Shippori Mincho'
+}
