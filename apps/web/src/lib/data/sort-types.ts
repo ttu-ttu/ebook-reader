aacdd1ed043f64c0d25edba5ac338e7d325837b5
Renji-xD <Renji-XD@users.noreply.github.com>
2023-01-13 17:53:16 +0100

    feat: add custom sort (#217)

---
+++ apps/web/src/lib/data/sort-types.ts
@@ -0,0 +1,17 @@
+/**
+ * @license BSD-3-Clause
+ * Copyright (c) 2023, ッツ Reader Authors
+ * All rights reserved.
+ */
+
+import type { BookCardProps } from '$lib/components/book-card/book-card-props';
+
+export enum SortDirection {
+  ASC = 'asc',
+  DESC = 'desc'
+}
+
+export interface SortOption {
+  property: Exclude<keyof BookCardProps, 'imagePath' | 'isPlaceholder'>;
+  direction: SortDirection;
+}
