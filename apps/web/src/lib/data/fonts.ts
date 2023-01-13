61abbdeb743055135d1200f90c11e20cfadef4a5
Renji-xD <trashmefull@hotmail.com>
2023-01-13 01:06:30 +0100

    feat: add klee one semibold variant

--- apps/web/src/lib/data/fonts.ts
+++ apps/web/src/lib/data/fonts.ts
@@ -7,6 +7,7 @@
 export enum LocalFont {
   GENEI = 'Genei Koburi Mincho v5',
   KLEEONE = 'Klee One',
+  KLEEONESEMIBOLD = 'Klee One SemiBold',
   NOTOSANSJP = 'Noto Sans JP',
   NOTOSERIFJP = 'Noto Serif JP',
   SHIPPORIMINCHO = 'Shippori Mincho'
