7555e247128f7a18daa43f01651d8bcc5cc6c931
SpazzTL <spazz.kr@gmail.com>
2025-08-09 18:11:48 -0400

    Fix url pathing ?

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,25 +1,30 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  compilerOptions: {
-    immutable: true
-  },
+    compilerOptions: {
+        immutable: true
+    },
 
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ],
+    preprocess: [
+        preprocess({
+            postcss: true
+        }),
+        vitePreprocess()
+    ],
 
-  kit: {
-    adapter: adapter({
-      fallback: '404.html'
-    })
-  }
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            // Add your repository name as the base path.
+            // This is crucial for GitHub Pages to resolve your links correctly.
+            base: process.env.NODE_ENV === 'production' ? '/korean-ebook-reader' : ''
+        }
+    }
 };
 
 export default config;
